85ff058d5dd5e51571b4a5f6c7b5097faf1c7691
413C5 <a_gonzalezzarate2@yahoo.com>
2023-05-31 16:26:04 -0600

    1.7: unicafe step2
    App is expanded to show more statistics about gathered feedback (all, average and positive)

--- part1/unicafe/src/components/App.js
+++ part1/unicafe/src/components/App.js
@@ -37,6 +37,15 @@ const App = () => {
             <p>
                 bad {bad}
             </p>
+            <p>
+                all {good + neutral + bad}
+            </p>
+            <p>
+                average {(good - bad) / (good + neutral + bad)}
+            </p>
+            <p>
+                positive {(good / (good + neutral + bad)) * 100} %
+            </p>
         </div>
     )
 }
