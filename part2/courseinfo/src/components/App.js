7e5f0d87e35249b35865137eef21a84658eb4ee7
413C5 <a_gonzalezzarate2@yahoo.com>
2023-06-02 12:09:22 -0600

    2.1: Course information step6
    Course component was defined, and renders the content , regardless of the n number of Parts

---
+++ part2/courseinfo/src/components/App.js
@@ -0,0 +1,71 @@
+import React from "react"
+
+const Header = ({ name }) => {
+    return (
+        <div>
+            <h1>{name}</h1>
+        </div>
+    )
+}
+
+const Content = ({ parts }) => {
+    return (
+        <div>
+            {
+                parts.map(part => {
+                    return (
+                        <Part key={part.id} part={part} />
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+const Part = ({ part }) => {
+    return (
+        <div>
+            <p>{part.name} {part.exercises}</p>
+        </div>
+    )
+}
+
+
+const Course = ({ course }) => {
+    return (
+        <div>
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+        </div>
+    )
+}
+
+const App = () => {
+    const course = {
+        id: 1,
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10,
+                id: 1
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7,
+                id: 2
+            },
+            {
+                name: 'State of a component',
+                exercises: 14,
+                id: 3
+            }
+        ]
+    }
+
+    return (
+        <Course course={course} />
+    )
+}
+
+export default App
