aa4f0647f7ffbe7e5d461662dffac549aa5f50b3
413C5 <a_gonzalezzarate2@yahoo.com>
2023-06-02 14:17:34 -0600

    2.4: Course information step9
    Now an arbitrary number of courses can be displayed

--- part2/courseinfo/src/components/App.js
+++ part2/courseinfo/src/components/App.js
@@ -3,7 +3,7 @@ import React from "react"
 const Header = ({ name }) => {
     return (
         <div>
-            <h1>{name}</h1>
+            <h2>{name}</h2>
         </div>
     )
 }
@@ -51,35 +51,62 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-    const course = {
-        id: 1,
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'State of a component',
-                exercises: 14,
-                id: 3
-            },
-            {
-                name: 'Redux',
-                exercises: 11,
-                id: 4
-            }
-        ]
-    }
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
     return (
-        <Course course={course} />
+        <div>
+            <h1>Web development curriculum</h1>
+            {
+                courses.map(course => {
+                    return (
+                        <Course key={course.id} course={course} />
+                    )
+                })
+            }
+        </div>
     )
 }
 
