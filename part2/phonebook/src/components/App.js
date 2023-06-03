173bcb7e4676bdefbedd56ac487e718c30537cbb
413C5 <a_gonzalezzarate2@yahoo.com>
2023-06-02 20:16:35 -0600

    2.11: The Phonebook Step6
    Data is fetched from axios server

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import PersonForm from "./PersonForm";
 import Persons from "./Persons";
 import Filter from "./Filter";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filter, setFilter] = useState('')
@@ -19,6 +15,17 @@ const App = () => {
         setNewName('')
         setNewNumber('')
     }
+
+    //Recover data
+    useEffect(() => {
+        axios
+          .get('http://localhost:3001/persons')
+          .then(response => {
+            console.log('entered effect')
+            setPersons(response.data)
+          })
+      }, [])
+    
 
     //handleAddPerson
     const addPerson = (event) => {
