9b7e44fba17b700b8223e05ef67926978eafe5f1
413C5 <a_gonzalezzarate2@yahoo.com>
2023-06-08 20:32:28 -0600

    3.3: Phonebook backend step3
    Added route to find 1 person

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -49,6 +49,21 @@ app.get('/info', (request, response) => {
         `</br> </br>${today} ${time}`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    //Converts string id into number
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    console.log(person)
+
+    if (person) {
+        response.json(person)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
